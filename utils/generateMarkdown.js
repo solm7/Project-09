e346fcc35a40832eb213682ff6d48ff61ef97aa4
solm7 <105757874+solm7@users.noreply.github.com>
2022-08-04 23:01:53 -0400

    Added 'renderLicencseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badgeURL;
+  switch (license) {
+  
+    case "No license":
+    badgeURL= ""
+    break;
+    case "Mozilla Public License 2.0":
+      badgeImageUrl = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    case "IBM Public License Version 1.0":
+      badgeImageUrl = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      break;
+    case "The Do What the Fuck You Want to Public License":
+      badgeURL = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+      break;
+    case "The Hippocratic License 2.1":
+      badgeImageUrl = "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)";
+      break;
+    default:
+      return "" 
+}
+  return badgeURL
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
